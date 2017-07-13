865adaeec50eb2d5ad2e96f8d97a57c1ef1a8ffa
Y4m1n <kungyamin@gmail.com>
2017-07-14 01:24:42 +0800

    style: 列表解析式

--- weekly.py
+++ weekly.py
@@ -12,23 +12,11 @@ for groups in xc_groups:
     for i in range(latested_cohort):
         df=df[~df.群名.str.contains("^{}S{}.+优达学城$".format(group_name,i))]
 
-# xc=df[df['群名'].str.contains(".+S.+优达学城$")]
-# zs=df[df['群名'].str.contains('正式学员')]
-# tg=df[df['群名'].str.contains("优等生$")]
-# hz=df[df['群名'].str.contains("Union A")]
-# dand=df[df['群名'].str.contains("数据分析")]
-# ipnd=df[df['群名'].str.contains("编程入门")]
-# fend=df[df['群名'].str.contains("前端开发")]
-# mlnd=df[df['群名'].str.contains("机器学习")]
-# dlnd=df[df['群名'].str.contains("深度学习")]
-# andr=df[df['群名'].str.contains("安卓")]
-#
-#
-# groups=[xc,zs,tg,hz,dand,fend,ipnd,mlnd,dlnd,andr]
-#
-#
-# for i in groups:
-#     print(len(i),i['入群人数'].sum(),i['退群人数'].sum(),i['在线用户数'].sum(),i['互动用户数'].sum(),i['互动用户率'].mean().round(2),i['互动次数'].sum())
+groups=[df[df['群名'].str.contains(key_word)] for key_word in [".+S.+优达学城$",'正式学员',"优等生$","Union A","数据分析",'编程入门',"前端开发","机器学习","深度学习",'安卓']]
+
+
+for i in groups:
+    print(len(i),i['入群人数'].sum(),i['退群人数'].sum(),i['在线用户数'].sum(),i['互动用户数'].sum(),i['互动用户率'].mean().round(2),i['互动次数'].sum())
 
 growths=df.loc[:,['群名','入群人数']]
 # python3 weekly.py | pbcopy- 别名wkl
