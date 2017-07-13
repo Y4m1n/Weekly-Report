8b929fdbb210e24af510e424abf755e25cd24f5d
Y4m1n <kungyamin@gmail.com>
2017-07-14 01:37:10 +0800

    add: ND通知群数据

--- weekly.py
+++ weekly.py
@@ -11,12 +11,13 @@ for groups in xc_groups:
     group_name=groups['群名'].values[0][:4]
     for i in range(latested_cohort):
         df=df[~df.群名.str.contains("^{}S{}.+优达学城$".format(group_name,i))]
+# 群分类
+groups=[df[df['群名'].str.contains(key_word)] for key_word in
+[".+S.+优达学城$",'正式学员',"优等生$","Union A","数据分析",'编程入门',"前端开发","机器学习","深度学习",'安卓',
+'数据分析S.+优达学城$','前端开发S.+优达学城$','机器学习S.+优达学城$','深度学习S.+优达学城$']]
 
-groups=[df[df['群名'].str.contains(key_word)] for key_word in [".+S.+优达学城$",'正式学员',"优等生$","Union A","数据分析",'编程入门',"前端开发","机器学习","深度学习",'安卓']]
-
-
+# 所需数据
 for i in groups:
     print(len(i),i['入群人数'].sum(),i['退群人数'].sum(),i['在线用户数'].sum(),i['互动用户数'].sum(),i['互动用户率'].mean().round(2),i['互动次数'].sum())
 
-growths=df.loc[:,['群名','入群人数']]
 # python3 weekly.py | pbcopy- 别名wkl
